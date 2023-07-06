57072b6d3ea2ae60b6b39c7cf8af68e948c1558b
Andrew <voropaev.andrey.grkkz@mail.ru>
2023-07-06 14:41:32 +0300

    feature: add requests +some tests 9

--- src/main/java/ru/practicum/shareit/comment/repository/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/comment/repository/CommentRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.comment.model.Comment;
 
 import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByItem_idOrderById(Long itemId);
